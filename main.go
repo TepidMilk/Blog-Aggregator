f102357e7dda8252cb473c1b2eb00e979088ba51
TepidMilk <ivorykingrey@gmail.com>
2025-05-12 13:46:07 -0500

    Update Main
    Run Read and SetUser functions and print the
    results of Read

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/tepidmilk/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println("error while reading config file: ", err)
+	}
+	cfg.SetUser("Noah")
+	fmt.Println(config.Read())
 }
