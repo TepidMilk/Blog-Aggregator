6b7dc60eefa2ebbbe9f9d4bcc15d757705d5ae94
TepidMilk <ivorykingrey@gmail.com>
2025-05-12 13:44:03 -0500

    Update config
    Export Read and SetUser commands and Add helper
    functions

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,64 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
+const (
+	configFileName = ".gatorconfig.json"
+)
+
 type Config struct {
-	db_url            string
-	current_user_name string
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	filePath := home + "/" + configFileName
+	return filePath, nil
+}
+
+func (c *Config) SetUser(user string) error {
+	c.CurrentUserName = user
+	return write(*c)
+}
+
+func write(cfg Config) error {
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filePath, data, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
 }
