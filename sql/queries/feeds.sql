8af819b25f30febc632b54bbe61d4e0587cc4d43
TepidMilk <ivorykingrey@gmail.com>
2025-05-15 16:00:41 -0500

    Add GetFeedByURL to feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,7 @@ RETURNING *;
 SELECT feeds.name, feeds.url, users.name
 FROM feeds INNER JOIN users
 ON feeds.user_id = users.id;
+
+-- name: GetFeedByURL :one
+SELECT id FROM feeds
+WHERE url = $1;
