c04619408945e676f2b8fa454819e212ed24fee0
TepidMilk <ivorykingrey@gmail.com>
2025-05-19 08:23:58 -0500

    Add GetPostsForUser query

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,11 @@ VALUES (
     $8
 )
 RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT posts.* FROM posts
+INNER JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+INNER JOIN users ON feed_follows.user_id = users.id
+WHERE users.id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
